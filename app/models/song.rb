be700827ea8000d5c70710fee7141f72fedeca4f
Sam Copeland <samcopeland7@gmail.com>
2016-02-09 10:14:52 -0500

    set associations between songs and artists

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,9 @@
 class Song < ActiveRecord::Base
+
+	belongs_to :artist
+
+	validates :name, presence: true, uniqueness: true
+	validates :artist_id, presence: true
+	validates :ratint, presence: true
+
 end
